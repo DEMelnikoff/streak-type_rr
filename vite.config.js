5cd0f82bb8a141fef868f233ab2a10efc17dc5a7
David E Melnikoff <davidemelnikoff@gmail.com>
2025-10-29 13:43:16 -0700

    @Deploy

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    base: "/streak-type_rr/",
+    build: {
+      target: "esnext",
+      sourcemap: true
+    },
+    test: {
+      environment: 'jsdom',
+      include: ['tests/*.{test,spec}.{js,ts}']
+    }
+  });
+  
