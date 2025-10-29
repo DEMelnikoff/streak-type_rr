fbcf5d7329bdd8ac2fdf6751bc4be0f99f47107c
David E Melnikoff <davidemelnikoff@gmail.com>
2025-10-29 13:55:51 -0700

    @Deploy

--- src/experiment.js
+++ src/experiment.js
@@ -6,7 +6,6 @@ import jsPsychSurveyLikert from '@jspsych/plugin-survey-likert';
 import jsPsychSurveyMultiChoice from '@jspsych/plugin-survey-multi-choice'; 
 //test 2
 
-
 import { readYaml, checkEmpty, fullScreenHandler, exportData, JSON2CSV, makeMultipliers } from "./utils";
 import {practicePhase, renderPlugin, bonusInstruction, bonusPhase, DICT} from "./jspsych-typing";
 
